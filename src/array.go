804a1d2f08719a1794ea61c10a7b15a48f185b15
zq2cjyy <lou-zhiqing@163.com>
2017-02-01 22:26:45 +0800

    add array.go

---
+++ src/array.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func modify(array [5]int) {
+	array[0] = 10
+	fmt.Println("In modify(),array value:", array)
+}
+func main() {
+	array := [5]int{1, 2, 3, 4, 5}
+	modify(array)
+	fmt.Println(array)
+}

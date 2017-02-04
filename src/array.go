7dcf31df0186c97f62bf2ac9edde81370bfd1c8b
LuZhiqiang <lou-zhiqing@163.com>
2017-02-05 00:49:08 +0800

    1

--- src/array.go
+++ src/array.go
@@ -7,10 +7,11 @@ import (
 func modify(array [5]int) {
 	array[0] = 10
 	fmt.Println("In modify(),array value:", array)
+
 }
 func main() {
 	array := [5]int{1, 2, 3, 4, 5}
 	modify(array)
 	fmt.Println(array)
-    fmt.Println("input over!")
+	fmt.Println("input over!")
 }

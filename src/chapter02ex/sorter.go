1b07ac89e0fdce4b4bc0125d7dba38f22563b18f
zq2cjyy <lou-zhiqing@163.com>
2017-02-14 17:45:23 +0800

    sort

--- src/chapter02ex/sorter.go
+++ src/chapter02ex/sorter.go
@@ -1,17 +1,59 @@
-package main
+package chapter02ex
 
 import (
-	"flag"
+	"bufio"
 	"fmt"
+	"io"
+	"os"
+	"strconv"
 )
 
-var infile *string = flag.String("i", "infile", "File contains values for sorting")
-var outfile *string = flag.String("o", "outfile", "File to receive sorted values")
-var algorithm *string = flag.String("a", "qsort", "Sort algorithm")
-
-func main() {
-	flag.Parse()
-	if infile != nil {
-		fmt.Println("infile=", *infile)
+func ReadValues(infile string) (values []int, err error) {
+	file, err := os.Open(infile)
+	if err != nil {
+		fmt.Println("文件打开失败")
+		return
 	}
+	defer file.Close()
+
+	br := bufio.NewReader(file)
+	values = make([]int, 0)
+	for {
+		line, isPrefix, err1 := br.ReadLine()
+
+		if err1 != nil {
+			if err1 != io.EOF {
+				err = err1
+			}
+			break
+		}
+
+		if isPrefix {
+			fmt.Println("太长了")
+			return
+		}
+		str := string(line)
+		value, err1 := strconv.Atoi(str)
+		if err1 != nil {
+			err = err1
+			return
+		}
+		values = append(values, value)
+	}
+	return
+}
+
+func WriteValues(values []int, outfile string) error {
+	file, err := os.Create(outfile)
+	if err != nil {
+		fmt.Println("文件创建失败")
+		return err
+	}
+	defer file.Close()
+
+	for _, value := range values {
+		str := strconv.Itoa(value)
+		file.WriteString(str + "\n")
+	}
+	return nil
 }
--- src/main/testMath.go
+++ src/main/testMath.go
@@ -1,16 +1,35 @@
 package main
 
 import (
-	"chapter02"
-	// "fmt"
+	"chapter02ex"
+	"fmt"
 	// "mymath"
+	"flag"
 )
 
-func main() {
+var infile *string = flag.String("i", "unsorted.dat", "File contains values for sorting")
+var outfile *string = flag.String("o", "sorted.dat", "File to receive sorted values")
+var algorithm *string = flag.String("a", "qsort", "Sort algorithm")
 
-	// var a int = 15
-	// var b int = 16
-	// c := mymath.Add(a, b)
-	// fmt.Println(c)
-	chapter02.Run()
+// func main() {
+
+// 	// var a int = 15
+// 	// var b int = 16
+// 	// c := mymath.Add(a, b)
+// 	// fmt.Println(c)
+// 	chapter02.Run()
+// }
+func main() {
+	flag.Parse()
+	if infile != nil {
+		fmt.Println("infile=", *infile)
+	}
+
+	values, err := chapter02ex.ReadValues(*infile)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(values)
+		chapter02ex.WriteValues(values, *outfile)
+	}
 }

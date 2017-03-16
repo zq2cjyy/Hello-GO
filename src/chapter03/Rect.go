413dd00e53c561ba18eb9fb25c85811766f31b74
luzhiqiang <lou-zhiqing@163.com>
2017-03-16 09:12:44 +0800

    test

--- src/chapter03/Rect.go
+++ src/chapter03/Rect.go
@@ -1,1 +1,21 @@
 package chapter03
+
+import "fmt"
+
+type Rect struct {
+	x, y          float64
+	height, width float64
+}
+
+func (rect *Rect) Area() float64 {
+	return rect.height * rect.width
+}
+
+func CreateRect(x, y, h, w float64) *Rect {
+	return &Rect{x, y, h, w}
+}
+
+func Run() {
+	rect1 := &Rect{0, 0, 100, 100}
+	fmt.Println(rect1.Area())
+}
